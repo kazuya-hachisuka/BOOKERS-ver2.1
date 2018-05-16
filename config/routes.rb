658d91de9c8039f969fde61080f2d26dd7e2ed49
kazuya-hachisuka <35261436+kazuya-hachisuka@users.noreply.github.com>
2018-05-16 13:57:49 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+
+
+  devise_for :users
+  root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   get '/' => 'books#top'
+  resources :users, only: [:show]
 end
